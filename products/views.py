9242023d9780f5f064b8d6c30e6819776b704b04
RussellWang <wtfbebez@gmail.com>
2025-07-16 22:20:54 +0800

    2025/07/16-2

--- products/views.py
+++ products/views.py
@@ -2,10 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Products
 
+
 def index(request):
     products = Products.objects.all()
-    return render(request, 'index.html', {'products':products})
+    return render(request, 'index.html', {'products': products})
 
 
 def new(request):
     return HttpResponse('New Products')
+
+
+def home(request):
+    return render(request, 'home.html')
--- pyshop/urls.py
+++ pyshop/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/', include('products.urls')),  # 把products的路由設為根目錄（空字串）
+    path('', views.home, name='home'),           # 新增首頁目錄頁
+    path('products/', include('products.urls')), 
 ]
